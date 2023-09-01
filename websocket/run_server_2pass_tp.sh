9c535664b047bd36a9c9c6fbbb53bda0b17b094c
dujing <dujing@xmov.ai>
2023-09-01 15:52:56 +0800

    add a timestamp model server.

--- websocket/run_server_2pass_tp.sh
+++ websocket/run_server_2pass_tp.sh
@@ -2,7 +2,7 @@
 build=build  # the build dir
 
 # Build websocket service, with onnxruntime
-if [ ! -f $build/bin/funasr-wss-server ]; then 
+if [ ! -f $build/bin/funasr-wss-server-2pass ]; then 
   echo "1st time run, we need to build the server, which may take a while(especially when fetch from git)."
   if [ ! -d ffmpeg-N-111383-g20b8688092-linux64-gpl-shared ]; then
     bash ../onnxruntime/third_party/download_ffmpeg.sh
@@ -23,18 +23,18 @@ download_model_dir="models"
 # now the hotword model and timestamp model is two different model
 # you can choose the following 3 model to use which you want, by comment the others.
 model_dir="damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-onnx"            # offline base model
-#model_dir="damo/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404-onnx" # hotword model
-#model_dir="damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-onnx"   # timestamp model
+model_dir="damo/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404-onnx" # hotword model
+model_dir="damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-onnx"   # timestamp model
 
 # the online model is better to use the small one, if you want to use the large, comment the small model line.
 online_model_dir="damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online-onnx"  # large online model
 online_model_dir="damo/speech_paraformer_asr_nat-zh-cn-16k-common-vocab8404-online-onnx"        # small online model
 vad_dir="damo/speech_fsmn_vad_zh-cn-16k-common-onnx"
-punc_dir="damo/punc_ct-transformer_zh-cn-common-vocab272727-onnx"
+punc_dir="damo/punc_ct-transformer_zh-cn-common-vad_realtime-vocab272727-onnx"
 itn_dir="damo/fst_itn_zh"
 decoder_thread_num=16
 io_thread_num=4
-port=10097
+port=10096
 certfile="../ssl_key/server.crt"
 keyfile="../ssl_key/server.key"
 
@@ -46,9 +46,10 @@ if [ ! -d $itn_real_path ]; then
   git clone https://www.modelscope.cn/thuduj12/fst_itn_zh.git $itn_real_path
 fi
 
-$build/bin/funasr-wss-server  \
+$build/bin/funasr-wss-server-2pass  \
   --download-model-dir ${download_model_dir} \
   --model-dir ${model_dir} \
+  --online-model-dir ${online_model_dir}  \
   --quantize true  \
   --vad-dir ${vad_dir} \
   --punc-dir ${punc_dir} \
