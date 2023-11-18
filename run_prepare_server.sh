1c2576eb18f280f86313298c7c5e66fe3389fb1b
dujing <dujing@xmov.ai>
2023-11-18 15:38:49 +0800

    添加ubuntu服务端必备的安装包。

--- run_prepare_server.sh
+++ run_prepare_server.sh
@@ -2,6 +2,8 @@
 
 ASR2PASS_ROOT="$(cd "$(dirname "$0")" && pwd)"
 
+sudo apt-get update && sudo apt-get install libopenblas-dev libssl-dev cmake build-essential
+
 echo "# 第1步, 编译并启动转写服务"
 cd $ASR2PASS_ROOT/websocket
 build=build  # the build dir
