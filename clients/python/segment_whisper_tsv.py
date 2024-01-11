533c32714e2cfe5c62d5de80171ee52992f826e4
dujing <dujing@xmov.ai>
2024-01-11 20:43:05 +0800

    更新按照Whisper转写结果进行切分的脚本。

--- clients/python/segment_whisper_tsv.py
+++ clients/python/segment_whisper_tsv.py
@@ -22,10 +22,10 @@ logger.addHandler(console_handler)
 # logger.addHandler(file_handler)
 
 
-thread_num = 2
+thread_num = 48
 timestamp_shift = 0   # 单位ms 经验值，片段结尾时间需要减去此值
 segment_merge_nums = 5   # whisper的转写结果是分段的，每段大约2s, 此值将多个小段合并拼成最后切分的一段音频。
-wav_scp="/data/audios/wav.scp0"
+wav_scp="/data/audios/wav.scp"
 transcript_path="/data/audios/whisper_transcript"
 segment_save_path="/data/audios/whisper_segments"
 
@@ -43,15 +43,30 @@ def process_scp(start_idx, num_files):
         wav_splits = wav.strip().split()
         wav_path = wav_splits[1] if len(wav_splits) > 1 else wav_splits[0]
         wav_name = os.path.basename(wav_path)
-        wav_name = os.path.splitext(wav_name)[0]      
+        wav_name = os.path.splitext(wav_name)[0]
+        
+        # 保存路径            
+        spker_id = os.path.splitext(wav_name)[0]
+        spker_dir = f"{segment_save_path}/{spker_id}"
+        if not os.path.exists(spker_dir):
+            os.makedirs(spker_dir)
+        text_file_path = f"{spker_dir}/transcription.txt"
+        if os.path.exists(text_file_path):
+            continue  # 已经切分过，跳过。
+              
         transcription = f"{transcript_path}/{wav_name}.tsv"
         if not os.path.exists(transcription):
             logging.warning(f"{transcription} not exist, continue.")
             continue
-        # 读取 JSON 文件
+        # 读取 tsv 文件
         with open(transcription) as tsv_file:
-            titles = tsv_file.readline()
             data = tsv_file.readlines()
+            if len(data) < 1:
+                continue
+            data = data[1:]  # 跳过第一行标题
+            if len(data) == 0:                                
+                logger.warning(f"wav_path:{wav_path}, transcription:{wav_name}.tsv is empty, continue.")
+                continue
 
         # 打开音频文件
         video = VideoFileClip(wav_path)
@@ -71,6 +86,9 @@ def process_scp(start_idx, num_files):
         # 遍历 JSON 数据
         for i, line in enumerate(data):
             paragraph = line.strip().split('\t')
+            if len(paragraph) !=3: # 没有转写内容
+                logger.warning(f"transcription:{wav_name}.tsv TSV format is not 'start\tend\ttext', check it.")
+                continue
             segment_start_time = float(paragraph[0])
             if start_time == None:  #  设置第一段时间起始点
                 start_time = segment_start_time
@@ -87,7 +105,8 @@ def process_scp(start_idx, num_files):
                 current_segment_text = ""  # 清空累计文本
 
         # 保存最后一个音频片段，因为总段数不是5的倍数，因此最后一段还没被保存 
-        if (i+1)%segment_merge_nums!=0 and start_time < segment_end_time and start_time/1000.0<audio.duration:
+        if (i+1)%segment_merge_nums!=0 and start_time is not None  \
+          and start_time < segment_end_time and start_time/1000.0<audio.duration:
             audio_segment = audio.subclip(start_time / 1000.0, segment_end_time / 1000.0)
             segments_duration += (segment_end_time-start_time)/1000.0
             audio_segments.append(audio_segment)    
@@ -99,12 +118,10 @@ def process_scp(start_idx, num_files):
         if not len(audio_segments) == len(paragraphs):
             logger.error(f"{wav_name}.tsv: audio segment and text length not equal.")
             continue
-            
-        spker_id = os.path.splitext(wav_name)[0]
-        spker_dir = f"{segment_save_path}/{spker_id}"
-        if not os.path.exists(spker_dir):
-            os.makedirs(spker_dir)
-        text_file_path = f"{spker_dir}/transcription.txt"
+        
+        if len(paragraphs) == 0:
+            continue
+        
         with open(text_file_path, 'w', encoding='utf-8') as fout:
             for i, segment in enumerate(audio_segments):
                 new_uttid = f"{spker_id}_{i:04d}"
